e18e30a99ce3c16bf5420689c994d0407d36495b
Kervin Fugata <151914582+kermuru@users.noreply.github.com>
2026-03-09 13:09:39 +0800

    remove google review, add instructions
    remove google review submission, add link to Renaissance Park and Chapel Review Page, and add clear instructions on how to leave a review

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,22 +1,22 @@
 export const environment = {
 
         //local
-//apiUrl: 'http://localhost:8000/api',
-
+        //apiUrl: 'http://localhost:8000/api',
 
         //old backend system
+
         //live
-apiUrl: 'https://springgreen-jellyfish-261481.hostingersite.com/api',
-
-        //staging
-       //apiUrl: 'https://wheat-capybara-997939.hostingersite.com/api',
-
+        apiUrl: 'https://springgreen-jellyfish-261481.hostingersite.com/api',
 
         //core-system backend
 
-        //live
-     // apiUrl: 'https://api.rp-vespera.cloud/api',
+        //staging
+        //apiUrl: 'https://wheat-capybara-997939.hostingersite.com/api',
 
-       //staging
-// apiUrl: 'https://staging.rp-vespera.cloud/api',
+        //live
+        // apiUrl: 'https://api.rp-vespera.cloud/api',
+
+        //staging
+        // apiUrl: 'https://staging.rp-vespera.cloud/api',
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,21 @@
 export const environment = {
+
+    //local
+    //apiUrl: 'http://localhost:8000/api',
+    
     //live
-apiUrl: 'https://springgreen-jellyfish-261481.hostingersite.com/api',
-
-      //local
-
- //
-//apiUrl: 'http://localhost:8000/api',
+    apiUrl: 'https://springgreen-jellyfish-261481.hostingersite.com/api',
 
     //staging
+
     //apiUrl: 'https://wheat-capybara-997939.hostingersite.com/api',
 
+    //core-system
 
+    //live
+    //apiUrl: 'https://api.rp-vespera.cloud/api',
+
+    //staging
+    //apiUrl: 'https://staging.rp-vespera.cloud/api',
     
-        //core-system
-
-            //live
-      //apiUrl: 'https://api.rp-vespera.cloud/api',
-
-       //staging
-  //apiUrl: 'https://staging.rp-vespera.cloud/api',
 };
